80e9a501ae7f3faea8276142b7aea716474613f3
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-15 08:27:09 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -21,7 +21,7 @@ def generate_restaurant_name_and_items(cuisine):
     # Chain 2: Menu Items
     prompt_template_items = PromptTemplate(
         input_variables=['restaurant_name'],
-        template="""Suggest ten menu items for pure vegiterranean {restaurant_name}. Return it as a comma separated string"""
+        template="""Suggest top ten menu items for pure vegiterraneans in {restaurant_name}."""
     )
 
     food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="menu_items")
