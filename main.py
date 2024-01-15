127531d48f0af1690805d8e66a33230bf234d9d5
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-15 08:37:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican"
 if cuisine:
     response = langchain_helper.generate_restaurant_name_and_items(cuisine)
     st.header(response['restaurant_name'].strip())
-    menu_items = response['menu_items'].strip().split(",")
+    menu_items = response['menu_items']
     st.write("**Menu Items**")
     for item in menu_items:
         st.write("-", item)
